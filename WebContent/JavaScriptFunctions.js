23db36802b45eebcf62721723271320ca2121f12
Carl O'Connor <oconnc25@tcd.ie>
2015-03-02 16:04:29 +0000

    update
    more drawing

--- WebContent/JavaScriptFunctions.js
+++ WebContent/JavaScriptFunctions.js
@@ -9,14 +9,18 @@ var colorPurple = "#cb3594";
 var colorGreen = "#659b41";
 var colorYellow = "#ffcf33";
 var colorBrown = "#986928";
-var curColor = colorPurple;
+var currColour = colorPurple;
 var clickColor = new Array();
 
 //functions!
+function hello(){
+	alert("hello world");
+}
+
 function myMousedown(e){
 	  var mouseX = e.pageX - this.offsetLeft;
 	  var mouseY = e.pageY - this.offsetTop;
-	
+	  
 	  paint = true;
 	  addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
 	  redraw();
@@ -63,7 +67,7 @@ function addClick(x, y, dragging){
   clickX.push(x);
   clickY.push(y);
   clickDrag.push(dragging);
-  clickColor.push(curColor);
+  clickColor.push(currColour);
 }
 
 function loadImage() {
@@ -71,4 +75,32 @@ function loadImage() {
     var ctx = c.getContext("2d");
     var img = document.getElementById("scream");
     ctx.drawImage(img,10,10);
+}
+
+function reset(){
+	context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+	clickX = new Array();
+	clickY = new Array();
+	clickDrag = new Array();
+	loadImage();
+}
+
+function setColour(){
+	 var r = document.getElementById("redRange").value;
+	 var b = document.getElementById("blueRange").value;
+	 var g = document.getElementById("greenRange").value;
+	 r = parseInt(r);
+	 b = parseInt(b);
+	 g = parseInt(g);
+	 //alert("Red: " + r + " Green: " + g + " Blue: " + b + " Hex: " + rgbToHex(r, g, b));
+	 currColour = rgbToHex(r, g, b);
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
